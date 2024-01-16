9bdfff61db62c7af9b043431ac6d395fd06d8cb4
masaki-yosida <yashukasono@gmail.com>
2024-01-16 16:36:22 +0900

    ユーザーの詳細画面作成　ログアウト機能使用不可

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,7 @@ class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
     @total_duration_by_title = calculate_total_duration_by_title(@tasks)
+    @users = User.all
 
     @new_task = Task.new
   end
