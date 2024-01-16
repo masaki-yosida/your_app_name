61f56c06b7e98fa70f0c8c013a47c27c398c0a13
masaki-yosida <yashukasono@gmail.com>
2024-01-16 10:20:49 +0900

    とりあえずdescription以外は登録で生きる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,28 @@
+# app/controllers/tasks_controller.rb
+
 class TasksController < ApplicationController
   def index
-    # ここに一覧表示のためのコードを追加
+    @tasks = Task.all
+    @new_task = Task.new
+  end
+
+  def create
+    @new_task = Task.new(task_params)
+
+if @new_task.save
+  redirect_to tasks_path, notice: 'タスクが作成されました。'
+else
+  Rails.logger.debug @new_task.errors.full_messages
+  @tasks = Task.all
+  render :index
+end
+end
+  
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :start_time, :end_time)
   end
 end
+
