83f254d75f83ccd08bd98ffe0b7d6746fec8f625
masaki-yosida <yashukasono@gmail.com>
2024-01-16 11:55:17 +0900

    作業時間の表示　ログアウトを使用可能に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def format_duration(seconds)
+    hours, remaining_seconds = seconds.divmod(3600)
+    minutes, seconds = remaining_seconds.divmod(60)
+
+    format('%02d:%02d', hours, minutes, seconds)
+  end
 end
