510cf6502524aced7dffa1a6bdca68de68621162
masaki-yosida <yashukasono@gmail.com>
2024-01-18 11:55:07 +0900

    デバイスと同じにしない為に消す

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { applications: 'applications' }
   resources :users
   resources :tasks
   root to: 'tasks#index'
