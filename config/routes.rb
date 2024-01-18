95dd573359bf258e0d7e2d3d6b2bf3f7d056e837
masaki-yosida <yashukasono@gmail.com>
2024-01-18 16:51:08 +0900

    Revert "消す"
    This reverts commit e7c6940e85dfd8c4a19017cbacfc2ffa4a2883bc.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :users do
-    resource :user_profile, only: [:new, :create, :edit, :update]
-  end
   
 end
