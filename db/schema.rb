96e3cef050aa3b6d138feebf8937f1a8e1c402f2
masaki-yosida <yashukasono@gmail.com>
2024-01-18 11:29:48 +0900

    Revert "消す"
    This reverts commit 47b771cf3fc6239f3f7d8b7926b986775f4ec1f8.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_18_021605) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_063731) do
   create_table "tasks", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -31,8 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_18_021605) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
